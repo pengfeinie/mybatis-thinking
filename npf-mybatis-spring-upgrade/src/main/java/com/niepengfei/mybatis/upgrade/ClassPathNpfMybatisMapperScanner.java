ff87866260f624a75b493b80bad091d32d94b712
pfnie <pfnie@yahoo.com>
2020-02-11 19:15:07 +0800

    ok

--- npf-mybatis-spring-upgrade/src/main/java/com/niepengfei/mybatis/upgrade/ClassPathNpfMybatisMapperScanner.java
+++ npf-mybatis-spring-upgrade/src/main/java/com/niepengfei/mybatis/upgrade/ClassPathNpfMybatisMapperScanner.java
@@ -2,7 +2,6 @@
 package com.niepengfei.mybatis.upgrade;
 
 import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
@@ -12,65 +11,46 @@ import java.util.Set;
 
 public class ClassPathNpfMybatisMapperScanner extends ClassPathBeanDefinitionScanner {
 
+    private Class<? extends MapperFactoryBean> mapperFactoryBeanClass = MapperFactoryBean.class;
 
-  private Class<? extends MapperFactoryBean> mapperFactoryBeanClass = MapperFactoryBean.class;
-
-  public ClassPathNpfMybatisMapperScanner(BeanDefinitionRegistry registry) {
-    super(registry, false);
-  }
-
-  public void setMapperFactoryBeanClass(Class<? extends MapperFactoryBean> mapperFactoryBeanClass) {
-    this.mapperFactoryBeanClass = mapperFactoryBeanClass == null ? MapperFactoryBean.class : mapperFactoryBeanClass;
-  }
-
-  public void registerFilters() {
-    addIncludeFilter((metadataReader, metadataReaderFactory) -> true);
-  }
-
-  /**
-   * Calls the parent search that will search and register all the candidates. Then the registered objects are post
-   * processed to set them as MapperFactoryBeans
-   */
-  @Override
-  public Set<BeanDefinitionHolder> doScan(String... basePackages) {
-    Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
-    if (beanDefinitions.isEmpty()) {
-    } else {
-      processBeanDefinitions(beanDefinitions);
+    public ClassPathNpfMybatisMapperScanner(BeanDefinitionRegistry registry) {
+        super(registry, false);
     }
-    return beanDefinitions;
-  }
 
-  private void processBeanDefinitions(Set<BeanDefinitionHolder> beanDefinitions) {
-    GenericBeanDefinition definition;
-    for (BeanDefinitionHolder holder : beanDefinitions) {
-      definition = (GenericBeanDefinition) holder.getBeanDefinition();
-      String beanClassName = definition.getBeanClassName();
-      // the mapper interface is the original class of the bean
-      // but, the actual class of the bean is MapperFactoryBean
-      definition.getConstructorArgumentValues().addGenericArgumentValue(beanClassName);
-      definition.setBeanClass(this.mapperFactoryBeanClass);
+    public void setMapperFactoryBeanClass(Class<? extends MapperFactoryBean> mapperFactoryBeanClass) {
+        this.mapperFactoryBeanClass = mapperFactoryBeanClass == null ? MapperFactoryBean.class : mapperFactoryBeanClass;
     }
-  }
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
-    return beanDefinition.getMetadata().isInterface() && beanDefinition.getMetadata().isIndependent();
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  protected boolean checkCandidate(String beanName, BeanDefinition beanDefinition) {
-    if (super.checkCandidate(beanName, beanDefinition)) {
-      return true;
-    } else {
-      return false;
+    /**
+     * Calls the parent search that will search and register all the candidates.
+     * Then the registered objects are post processed to set them as MapperFactoryBeans
+     */
+    @Override
+    public Set<BeanDefinitionHolder> doScan(String... basePackages) {
+      Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
+      if (!beanDefinitions.isEmpty()) {
+        processBeanDefinitions(beanDefinitions);
+      }
+      return beanDefinitions;
     }
-  }
 
+    private void processBeanDefinitions(Set<BeanDefinitionHolder> beanDefinitions) {
+        for (BeanDefinitionHolder holder : beanDefinitions) {
+            GenericBeanDefinition definition = (GenericBeanDefinition) holder.getBeanDefinition();
+            String beanClassName = definition.getBeanClassName();
+            // the mapper interface is the original class of the bean
+            // but, the actual class of the bean is MapperFactoryBean
+            definition.setBeanClass(this.mapperFactoryBeanClass);
+            definition.getConstructorArgumentValues().addGenericArgumentValue(beanClassName);
+        }
+    }
+
+    @Override
+    protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+      return beanDefinition.getMetadata().isInterface() && beanDefinition.getMetadata().isIndependent();
+    }
+
+    public void registerFilters() {
+        addIncludeFilter((metadataReader, metadataReaderFactory) -> true);
+    }
 }
