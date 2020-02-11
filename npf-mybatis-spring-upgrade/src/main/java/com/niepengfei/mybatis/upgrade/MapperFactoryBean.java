0736540d229c23b7fbf5e9ec4ed716bbf8ca8d6d
pfnie <pfnie@yahoo.com>
2020-02-11 19:03:59 +0800

    remove

--- npf-mybatis-spring-upgrade/src/main/java/com/niepengfei/mybatis/upgrade/MapperFactoryBean.java
+++ npf-mybatis-spring-upgrade/src/main/java/com/niepengfei/mybatis/upgrade/MapperFactoryBean.java
@@ -3,46 +3,30 @@ package com.niepengfei.mybatis.upgrade;
 
 
 import org.springframework.beans.factory.FactoryBean;
-
 import java.lang.reflect.Proxy;
 
 public class MapperFactoryBean<T> implements FactoryBean<T> {
 
   private Class<T> mapperInterface;
 
-  public MapperFactoryBean() {
-
-  }
-
   public MapperFactoryBean(Class<T> mapperInterface) {
     this.mapperInterface = mapperInterface;
   }
 
-
-
-  /**
-   * {@inheritDoc}
-   */
   @Override
   public T getObject() throws Exception {
-    return (T)Proxy.newProxyInstance(mapperInterface.getClassLoader(),
+     return (T)Proxy.newProxyInstance(mapperInterface.getClassLoader(),
             new Class[]{mapperInterface},new NpfInvocationHandler());
   }
 
-  /**
-   * {@inheritDoc}
-   */
   @Override
   public Class<T> getObjectType() {
-    return this.mapperInterface;
+     return this.mapperInterface;
   }
 
-  /**
-   * {@inheritDoc}
-   */
   @Override
   public boolean isSingleton() {
-    return true;
+     return true;
   }
 
 }
